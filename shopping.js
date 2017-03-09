ce5fc87118e895f4d4c668b96ae634c2936517ac
Ali Diyab <alidiyab1997@gmail.com>
2017-03-09 14:16:43 +0300

    Done.

--- shopping.js
+++ shopping.js
@@ -1,3 +1,5 @@
+var cart = []
+
 function total() {
   let t = 0
 
@@ -6,6 +8,43 @@ function total() {
       t += cart[i][item]
     }
   }
-
   return t
 }
+
+
+function setCart(array){
+  cart = array;
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+var price = randomPrice();
+cart.push('{item}: price')
+console.log(`${item} has been added to your cart.`)
+return cart;
+}
+
+function randomPrice() {
+  var random = Math.random() * 100
+  return Math.floor(random);
+}
+
+
+ function viewCart() {
+  var array = []
+   for (let i = 0, l = cart.length; i < l; i++) {
+    var item = Object.keys(cart[i])[0]
+    var price = cart[i][item]
+    var string = `${item} at $${price}`
+     array.push(string)
+  }
+  if (cart.length===0){
+    console.log("Your shopping cart is empty.")
+  }
+  else {
+   console.log(`In your cart, you have ${array.join(', ')}.`)
+  }
+ }
